c737025f664a27d0e0aa1351d7a96edb80616c8d
Vedat Yilmiz <mr.yilmaz@mondriaanict.nl>
2018-11-16 11:59:19 +0100

    Add project files.

---
+++ quiz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question q = new Question();
+            q.text = "Wie was de uitvinder van adnan?";
+            q.answer = "God";
+            q.display();
+            q.ReadAnswer();
+
+            string s =  q.uAnswer;
+            Console.WriteLine("You antwoord is:" + s);
+            Console.WriteLine(q.checkAnswer(s));
+            Console.ReadLine();
+
+            Question q2 = new Question();
+            q2.text = "Wie van deze mensen is gay? Selecteer A,B,C,D";
+            Console.WriteLine("A:Colin");
+            Console.WriteLine("B:Tom");
+            Console.WriteLine("C:Abdel");
+            Console.WriteLine("D:Sugar Lee Whooper");
+
+            q2.answer = "B";
+            q2.display();
+            q2.ReadAnswer();
+            string s2 = q2.answer;
+            Console.WriteLine("You antwoord is:" + s2);
+            Console.WriteLine(q2.checkAnswer(s2));
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ quiz/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quiz
+{
+    class Question
+    {
+       public string text{ get; set; }
+       public string answer{ get; set; }
+       public string uAnswer{ get; set; }
+
+        public Question()
+        {
+            text = "";
+            answer = "";
+        }
+        public Boolean checkAnswer(String response)
+        {
+            return response.Equals(answer);
+        }
+        public void display()
+        {
+            Console.WriteLine(this.text);
+        }
+        public void ReadAnswer()
+        {
+             uAnswer = Console.ReadLine();
+        }
+    }
+}
